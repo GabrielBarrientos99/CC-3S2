be58b0fbbfa87e94f96437ba31968257eaccbba8
GabrielBarrientos99 <129352498+GabrielBarrientos99@users.noreply.github.com>
2023-06-21 09:54:54 -0500

    Add files via upload

---
+++ PracticaCalificada4/PruebasEstructurales/PruebasEstructurales/src/test/java/PruebasEstructurales/CountWordsTest.java
@@ -0,0 +1,27 @@
+package PruebasEstructurales;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class CountWordsTest {
+    @Test
+    void twoWordsEndingWithS() { //1
+        int words = new CountWords().count("dogs cats");
+        assertThat(words).isEqualTo(2);
+    }
+
+    @Test
+    void noWordsAtAll() {
+        int words = new CountWords().count("dog cat");
+        assertThat(words).isEqualTo(0);
+    }
+
+    @Test
+    void wordsThatEndInR(){
+        int words = new CountWords().count("car bar");
+        assertThat(words).isEqualTo(2);
+    }
+
+}
